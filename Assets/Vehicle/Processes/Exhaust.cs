1b509eca6d2f235f8a2a1225c26e0593590c4295
Aarchble <thomascollins99@me.com>
2022-03-30 07:35:26 +1000

    Stopped plume from reflecting on itself when x is too large

--- Assets/Vehicle/Processes/Exhaust.cs
+++ Assets/Vehicle/Processes/Exhaust.cs
@@ -7,7 +7,7 @@ public class Exhaust : Expansion
     Parcel Exit;
     float NozzleAngle;
     float NozzleRadius;
-    Vector3[] jetBoundary;
+    List<Vector3> jetBoundary;
     bool Upper;
 
     public Exhaust(Parcel e, float nozzleAngle, float nozzleRadius, bool upper = false)
@@ -24,8 +24,8 @@ public class Exhaust : Expansion
         float dr = 0.1f;
         int n = 10;
 
-        jetBoundary = new Vector3[n];
-        jetBoundary[0] = new Vector3(0f, 1f);
+        jetBoundary = new();
+        jetBoundary.Add(new Vector3(0f, 1f));
         float M = Exit.M;
 
         float ARe = SonicArea(i.Gamma, Exit.M);
@@ -33,11 +33,19 @@ public class Exhaust : Expansion
 
         for (int seg = 1; seg < n; seg++)
         {
-            jetBoundary[seg] = new Vector3(0f, jetBoundary[seg - 1].y + dr);
-            float AR = jetBoundary[seg].y * jetBoundary[seg].y * ARe;
+            Vector3 newPoint = new Vector3(0f, jetBoundary[seg - 1].y + dr);
+            float AR = newPoint.y * newPoint.y * ARe;
             M = SonicAreaMach(i.Gamma, AR);
             float theta = PrandtlMeyerAngle(i.Gamma, M) - PrandtlMeyerAngle(Exit.Gamma, Exit.M);
-            jetBoundary[seg].x = dr / Mathf.Tan(alpha - theta) + jetBoundary[seg - 1].x;
+            newPoint.x = dr / Mathf.Tan(alpha - theta) + jetBoundary[seg - 1].x;
+            if (newPoint.x < jetBoundary[seg - 1].x)
+            {
+                break;
+            }
+            else
+            {
+                jetBoundary.Add(newPoint);
+            }
         }
 
         float Tratio = (1f + (i.Gamma - 1f) / 2f * i.M * i.M) / (1f + (i.Gamma - 1f) / 2f * M * M);
@@ -54,10 +62,10 @@ public class Exhaust : Expansion
         float invert = Upper ? 1f : -1f;
 
         Vector3 outletOrigin = nearStream.Outlet[0] + (nearStream.Outlet[^1] - nearStream.Outlet[0]) * 0.5f;
-        featureVertices = new Vector3[jetBoundary.Length];
-        Mesh[] meshes = new Mesh[jetBoundary.Length - 1];
+        featureVertices = new Vector3[jetBoundary.Count];
+        Mesh[] meshes = new Mesh[jetBoundary.Count - 1];
 
-        for (int vert = 0; vert < jetBoundary.Length; vert++)
+        for (int vert = 0; vert < jetBoundary.Count; vert++)
         {
             featureVertices[vert] = outletOrigin + jetBoundary[vert].x * NozzleRadius * nearStream.FlowDir + invert * jetBoundary[vert].y * NozzleRadius * Vector3.Cross(nearStream.FlowDir, Vector3.forward);
 
